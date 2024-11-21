edc841e52bfe14b43cedd20ca726eb107015c197
Mariusz <klimcio@gmail.com>
2024-11-21 16:20:54 +0100

    View a simple window!

--- RayLib_The_Beginning/RayLib_The_Beginning/Program.cs
+++ RayLib_The_Beginning/RayLib_The_Beginning/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Raylib_cs;
+
+Raylib.InitWindow(800, 480, "Hello World");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
