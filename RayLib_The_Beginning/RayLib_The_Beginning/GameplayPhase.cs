52116e294dbb455a3aa682a795e08001a903034c
Mariusz <klimcio@gmail.com>
2024-12-04 22:49:36 +0100

    Move protagonist logic into new class

--- RayLib_The_Beginning/RayLib_The_Beginning/GameplayPhase.cs
+++ RayLib_The_Beginning/RayLib_The_Beginning/GameplayPhase.cs
@@ -1,9 +1,23 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-public class GameplayPhase : IGamePhase
+public class Protagonist
 {
     Vector2 ballPosition = new((float)Program.width / 2, (float)Program.height / 2);
+
+    public void Draw()
+        => Raylib.DrawCircleV(ballPosition, 50, Color.Maroon);
+
+    public void MoveHorizontally(float newH)
+        => ballPosition.X += newH;
+
+    public void MoveVertically(float newH)
+        => ballPosition.Y += newH;
+}
+
+public class GameplayPhase : IGamePhase
+{
+    Protagonist protagonist = new();
 
     public void Draw()
     {
@@ -11,15 +25,15 @@ public class GameplayPhase : IGamePhase
 
         Raylib.DrawText("move the ball with arrow keys", 10, 10, 20, Color.DarkGray);
 
-        Raylib.DrawCircleV(ballPosition, 50, Color.Maroon);
+        protagonist.Draw();
     }
 
     public void Update()
     {
-        if (Raylib.IsKeyDown(KeyboardKey.Right)) ballPosition.X += 2.0f;
-        if (Raylib.IsKeyDown(KeyboardKey.Left)) ballPosition.X -= 2.0f;
-        if (Raylib.IsKeyDown(KeyboardKey.Up)) ballPosition.Y -= 2.0f;
-        if (Raylib.IsKeyDown(KeyboardKey.Down)) ballPosition.Y += 2.0f;
+        if (Raylib.IsKeyDown(KeyboardKey.Right)) protagonist.MoveHorizontally(2.0f);
+        if (Raylib.IsKeyDown(KeyboardKey.Left)) protagonist.MoveHorizontally(-2.0f);
+        if (Raylib.IsKeyDown(KeyboardKey.Up)) protagonist.MoveVertically(-2.0f);
+        if (Raylib.IsKeyDown(KeyboardKey.Down)) protagonist.MoveVertically(2.0f);
 
         if (Raylib.IsKeyPressed(KeyboardKey.Escape))
         {
